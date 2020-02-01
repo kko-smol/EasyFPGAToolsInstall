50b1b2c5358a66c363914c76da50fa6607481ef1
ArsenioDev <ArsenioDev@users.noreply.github.com>
2020-02-01 00:06:22 -0500

    Update gowintools.sh

--- gowintools.sh
+++ gowintools.sh
@@ -12,7 +12,7 @@ if [[ "$UNAME_STR" == "Darwin" ]] && hash brew 2>/dev/null; then
 fi
 if [[ "$UNAME_STR" == "Linux" ]] && hash apt-get 2>/dev/null; then
 	sudo apt-get install -y pkg-config build-essential cmake bison autoconf flex gawk tcl-dev libffi-dev git mercurial python python3 python3-dev libfl-dev libboost-dev libboost-filesystem-dev libboost-thread-dev libboost-program-options-dev libboost-python-dev qt5-default libftdi-dev
-  sudo apt-get install -y libreadline-dev clang graphviz xdot 
+  sudo apt-get install -y libreadline-dev clang graphviz xdot libftdi1-2 libftdi1-dev libftdipp1-3 libftdipp1-dev libudev-dev cmake
 fi
 
 echo "┌───────────────────┐"
